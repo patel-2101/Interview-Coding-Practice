9112e1edcbe881417f96763cc42f17395218374b
Ravi Patel <patelravi2253@gmail.com>
2026-07-16 10:41:01 +0530

    que_18_LinearSearch

--- que_18_LinearSearch.java
+++ que_18_LinearSearch.java
@@ -6,7 +6,18 @@ public class que_18_LinearSearch {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of array : ");
         int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("nter the target : ");
+        int t = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == t) {
+                System.out.println("Index is : " + i + "Data is : " + arr[i]);
+            }
+        }
 
-        for()
+
     }
 }
