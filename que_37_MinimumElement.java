d971a364c36d4e8d1ac0f942c023ed058b8e63a6
Ravi Patel <patelravi2253@gmail.com>
2026-07-17 22:57:06 +0530

    que_37_MinimumElement

---
+++ que_37_MinimumElement.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class que_37_MinimumElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array : ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter the value in Array : ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]<min) {
+                min = arr[i];
+            }
+        }
+        System.out.println("Min number is : "+min);
+    }
+
+}
