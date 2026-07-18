4c31228f0c0aacd31ffba4b1df52aea51bc19c6c
Ravi Patel <patelravi2253@gmail.com>
2026-07-18 10:58:17 +0530

    que_02_PrimeNumber

--- que_02_PrimeNumber.java
+++ que_02_PrimeNumber.java
@@ -4,6 +4,22 @@ public class que_02_PrimeNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        if (n<=1) {
+            System.out.println("Not a prime number : ");
+            return;
+        }
+        int org = n;
+        for (int i = 2; i < n/2; i++) {
+            if (n%i == 0) {
+                System.out.println("Not a prime number  : " + org);
+                return;
+            }
+        }
+
+        System.out.println("Prime number : " + n);
+
+
+
         
     }
 }
