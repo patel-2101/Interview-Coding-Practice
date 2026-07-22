ccd33f61a5844cdbd3a6803378b7bdd60a2b6dc5
Ravi Patel <patelravi2253@gmail.com>
2026-07-22 13:24:32 +0530

    que_39_PowerOfNumber

---
+++ que_39_PowerOfNumber.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+
+public class que_39_PowerOfNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double x = sc.nextDouble(); // Read a double
+        int n = sc.nextInt();
+        double res = Math.pow(x, n);
+        System.out.println(res);
+        
+    }
+}
