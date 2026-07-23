d75e39d7321aabfd5f2e1cf61427d56150471d0c
Ravi Patel <patelravi2253@gmail.com>
2026-07-24 00:27:55 +0530

    que_38_RightTrianglePattern

--- que_38_RightTrianglePattern.java
+++ que_38_RightTrianglePattern.java
@@ -5,6 +5,12 @@ public class que_38_RightTrianglePattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int m = sc.nextInt();
-        int n = sc.nextInt();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
+
--- que_52_arrayList.java
+++ que_52_arrayList.java
@@ -1,3 +1,31 @@
+import java.util.Arrays;
+
 public class que_52_arrayList {
-    
-}
+    public static void main(String[] args) {
+
+        int arr[][][] = {
+            {
+                {1, 2, 3, 4},
+                {7, 8, 9, 5},
+                {9, 3, 2, 8, 7, 2}
+            }
+        };
+
+        System.out.println(Arrays.deepToString(arr));
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                for (int k = 0; k < arr[i][j].length; k++) {
+                    System.out.print(arr[i][j][k] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+}        
+
+
+
+
+
